4950b2050720990ed406fdc8046025c8ef4f0188
solarburst <ausesin@gmail.com>
2022-06-26 13:53:47 +0700

    functional

--- bulletin-board-front/src/pages/AdPage/index.jsx
+++ bulletin-board-front/src/pages/AdPage/index.jsx
@@ -1,78 +1,95 @@
-import React, {useEffect} from "react";
-import {useNavigate, useParams} from "react-router-dom";
-import {ModalProvider} from "../../context/ModalContext";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ModalProvider } from "../../context/ModalContext";
 import DefaultLayout from "../../layouts/DefaultLayout";
 import styles from "./index.module.css";
-import {useDispatch, useSelector} from "react-redux";
-import {deletePhotoById, getPhotoById} from "../../redux/features/posts/postSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deletePhotoById,
+  getPhotoById,
+} from "../../redux/features/posts/postSlice";
 
-import {useLocalStorage} from '../../hooks/useLocalStorage'
-import {toast} from "react-toastify";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { toast } from "react-toastify";
 
 const AdPage = () => {
-	
-	const id = useParams()
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	
-	const {photo, isLoading, isError} = useSelector(state => state.posts)
-	
-	const {getValueFromLocalStorage} = useLocalStorage()
-	
-	const clientId = photo.advertisement?.client.entityId
-	const userId = getValueFromLocalStorage('userId')
-	const userRole = getValueFromLocalStorage('role')
-	
-	useEffect(() => {
-		dispatch(getPhotoById({entityId: id.id}))
-	}, [])
-	
-	if (isLoading) return <p>loading</p>
-	
-	const onDelete = () => {
-		if (window.confirm('delete ad?') && (Number(userId) === Number(clientId) || userRole === 'ADMIN')) {
-			dispatch(deletePhotoById(id.id))
-			navigate('/')
-		} else {
-			toast('У вас нет привелегий')
-		}
-		
-	}
-	
-	return (
-		<ModalProvider>
-			<DefaultLayout>
-				<div className="container">
-					<>
-						<div className={styles.ad}>
-							<div className={styles.image}>
-								<img src={photo.advertisement?.photo?.urn}/>
-							</div>
-							<div className={styles.info}>
-								<div className={styles.name}>{photo.advertisement?.name}</div>
-								<div className={styles.price}>{photo.advertisement?.price} руб.</div>
-								<div className={styles.date}>{photo.advertisement?.createDate}</div>
-							</div>
-							<div className={styles.contacts}>
-								<div className={styles.number}>Phone: {photo.advertisement?.client.phoneNumber}</div>
-								<div className={styles.number}>House: {photo.advertisement?.houseNumber}</div>
-							</div>
-							<div className={styles.buttons}>
-								<div className={styles.edit}>Редактировать</div>
-								<div className={styles.delete} onClick={onDelete}>
-									Удалить
-								</div>
-							</div>
-						</div>
-					</>
-				</div>
-			</DefaultLayout>
-		</ModalProvider>
-	);
+  const id = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { photo, isLoading, isError } = useSelector((state) => state.posts);
+
+  const { getValueFromLocalStorage } = useLocalStorage();
+
+  const clientId = photo.advertisement?.client.entityId;
+  const userId = getValueFromLocalStorage("userId");
+  const userRole = getValueFromLocalStorage("role");
+
+  useEffect(() => {
+    dispatch(getPhotoById({ entityId: id.id }));
+  }, []);
+
+  if (isLoading) return <p>loading</p>;
+
+  const onDelete = () => {
+    if (
+      window.confirm("Удалить объявление?") &&
+      (Number(userId) === Number(clientId) || userRole === "ADMIN")
+    ) {
+      dispatch(deletePhotoById(id.id));
+      navigate("/");
+    } else {
+      toast("У вас нет привелегий");
+    }
+  };
+
+  return (
+    <ModalProvider>
+      <DefaultLayout>
+        <div className="container">
+          <>
+            <div className={styles.ad}>
+              <div className={styles.image}>
+                <img src={photo.advertisement?.photo?.urn} />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.name}>{photo.advertisement?.name}</div>
+                <div className={styles.price}>
+                  {photo.advertisement?.price} руб.
+                </div>
+                <div className={styles.date}>
+                  {photo.advertisement?.createDate}
+                </div>
+                <div className={styles.description}>
+                  {photo.advertisement?.description}
+                </div>
+              </div>
+              <div className={styles.contacts}>
+                <div className={styles.number}>
+                  Телефон: {photo.advertisement?.client.phoneNumber}
+                </div>
+                <div className={styles.number}>
+                  Имя: {photo.advertisement?.client.name}
+                </div>
+                <div className={styles.number}>
+                  Адрес: {photo.advertisement?.locality?.region?.name},
+                  {photo.advertisement?.locality?.name},
+                  {photo.advertisement?.street?.name},
+                  {photo.advertisement?.houseNumber}
+                </div>
+              </div>
+              <div className={styles.buttons}>
+                <div className={styles.edit}>Редактировать</div>
+                <div className={styles.delete} onClick={onDelete}>
+                  Удалить
+                </div>
+              </div>
+            </div>
+          </>
+        </div>
+      </DefaultLayout>
+    </ModalProvider>
+  );
 };
 
 export default AdPage;
-
-
-
-
--- bulletin-board-front/src/pages/HomePage/index.jsx
+++ bulletin-board-front/src/pages/HomePage/index.jsx
@@ -1,44 +1,50 @@
-import React, {useCallback, useEffect} from "react";
-import {Link} from "react-router-dom";
-import {ModalProvider} from "../../context/ModalContext";
+import React, { useCallback, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { ModalProvider } from "../../context/ModalContext";
 import DefaultLayout from "../../layouts/DefaultLayout";
-import {useDispatch, useSelector} from "react-redux";
-import {getPhotos} from "../../redux/features/posts/postSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getPhotos } from "../../redux/features/posts/postSlice";
+import styles from "./index.module.css";
 
 const HomePage = () => {
-	
-	const dispatch = useDispatch()
-	const {photos} = useSelector(state => state.posts)
-	
-	
-	console.log(photos)
-	
-	const getDeviceDataTotal = useCallback(() => {
-		dispatch(getPhotos())
-	}, []);
-	
-	useEffect(() => {
-		getDeviceDataTotal()
-	}, [getDeviceDataTotal]);
-	
-	return (
-		<ModalProvider>
-			<DefaultLayout>
-				<div className="container">
-					<div className="cards">
-						{photos.advertisementList?.map((ad) => (
-							<Link className="card" to={`cards/${ad.entityId}`}>
-								<img src={ad.photo.urn} alt=""/>
-								<div>
-									<h1>{ad.name}</h1>
-								</div>
-							</Link>
-						))}
-					</div>
-				</div>
-			</DefaultLayout>
-		</ModalProvider>
-	);
+  const dispatch = useDispatch();
+  const { photos } = useSelector((state) => state.posts);
+
+  console.log(photos);
+
+  const getDeviceDataTotal = useCallback(() => {
+    dispatch(getPhotos());
+  }, []);
+
+  useEffect(() => {
+    getDeviceDataTotal();
+  }, [getDeviceDataTotal]);
+
+  return (
+    <ModalProvider>
+      <DefaultLayout>
+        <div className="container">
+          <div className="cards">
+            {photos.advertisementList?.map((ad) => (
+              <Link
+                className={styles.card}
+                to={`cards/${ad.entityId}`}
+                key={ad.entityId}
+              >
+                <img src={ad.photo.urn} alt="" />
+                <div className={styles.info}>
+                  <h3>{ad.name}</h3>
+                  <h4 className={styles.price}>{ad.price} руб.</h4>
+                  <div className={styles.locality}>{ad.locality?.name}</div>
+                  <div className={styles.date}>{ad.createDate}</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </DefaultLayout>
+    </ModalProvider>
+  );
 };
 
 export default HomePage;
